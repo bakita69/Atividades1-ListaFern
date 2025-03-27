071bcb4bf10ae9da065d69a144d52f5d98b0afe3
Cauã Dérek <174913858+bakita69@users.noreply.github.com>
2025-03-27 14:25:25 -0300

    Atividade18

---
+++ Atividade18/src/atividade18/Atividade18.java
@@ -0,0 +1,13 @@
+
+package atividade18;
+
+public class Atividade18 {
+
+    public static void main(String[] args) {
+        int x;
+        x = 10;       
+        System.out.println(x); 
+        System.out.println("O valor de x = "+ (x+1));
+    }
+    
+}
