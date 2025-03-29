81c67bb31993f2e05821b553aadfc475dd1f951a
Cauã Dérek <174913858+bakita69@users.noreply.github.com>
2025-03-29 01:09:27 -0300

    Atividade31

---
+++ Atividade31/src/atividade31/Atividade31.java
@@ -0,0 +1,13 @@
+
+package atividade31;
+
+public class Atividade31 {
+
+    public static void main(String[] args) {
+        int ma;
+        
+        ma =(8+9+7) / 3;
+        System.out.println("\n A media arimetica e: " + ma);
+        System.out.println("\n");        
+    }   
+}
