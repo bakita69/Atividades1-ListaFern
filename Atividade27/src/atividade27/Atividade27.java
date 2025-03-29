af6a706401c95adda9c72ddb587abd1062bd3ef8
Cauã Dérek <174913858+bakita69@users.noreply.github.com>
2025-03-29 00:51:07 -0300

    Atividade27

---
+++ Atividade27/src/atividade27/Atividade27.java
@@ -0,0 +1,21 @@
+
+package atividade27;
+
+public class Atividade27 {
+
+    public static void main(String[] args) {
+        int x,y;
+        
+        x = 2;
+        y = 5;
+        
+        System.out.println("\n x = " + x);
+        x++;
+        System.out.println("\n y = " + y);
+        y--;
+        
+        System.out.println("\n Novo valor de x = "+ x);
+        System.out.println("\n Novo valor de y = "+ y);
+        System.out.println("\n\n");               
+    }    
+}
